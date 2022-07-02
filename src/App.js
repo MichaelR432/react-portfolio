3ebd2342e9ffda2730ebfeb70342dc28f7fb7ae0
Michael Ramos <michaelaramos182@gmail.com>
2022-07-01 23:10:10 -0500

    "Adding in untracked files."

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation/Navigation';
+import Home from './pages/Home/Home';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation/>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='#Main'></Route>
+        <Route path='#about'></Route>
+        <Route path='#work'></Route>
+        <Route path='#contact'></Route>
+      </Routes>
+    </Router>
   );
 }
 
